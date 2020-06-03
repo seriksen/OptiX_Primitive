0f7cae7a322ada4c2579c3952f9df7ec0785436c
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-06-03 02:04:44 +0100

    Testing OptiXSphere

--- OptiXSphere/OptiXSphere.cc
+++ OptiXSphere/OptiXSphere.cc
@@ -189,7 +189,7 @@ int main(int argc, char** argv)
 
   box->setPrimitiveCount( 1u );
   box->setBoundingBoxProgram( context->createProgramFromPTXFile( box_ptx , "bounds" ) );
-  box->setIntersectionProgram( context->createProgramFromPTXFile( box_ptx , "robust_intersect" ) ) ;
+  box->setIntersectionProgram( context->createProgramFromPTXFile( box_ptx , "intersect" ) ) ;
 
   float sz = ce.w ;
   box["boxmin"]->setFloat( -sz/2.f, -sz/2.f, -sz/2.f );
