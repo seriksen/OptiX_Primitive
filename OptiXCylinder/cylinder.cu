d1bb0bd2ef0e0cea1706d98436df355f08090ecd
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-06-08 02:58:03 +0100

    Add cylinder back

--- OptiXCylinder/cylinder.cu
+++ OptiXCylinder/cylinder.cu
@@ -3,109 +3,129 @@
 #include <optixu/optixu_matrix_namespace.h>
 #include <optixu/optixu_aabb_namespace.h>
 
-/*
- * Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of NVIDIA CORPORATION nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <optix_world.h>
-
 using namespace optix;
 
-rtDeclareVariable(float4,  sphere, , );
-
+// Communication Variables
+rtDeclareVariable(float3, cylinder_min, , );
+rtDeclareVariable(float3, cylinder_max, , );
+rtDeclareVariable(float3, cylinder_r, , );
+rtDeclareVariable(optix::Ray, ray, rtCurrentRay, );
+rtDeclareVariable(float3, texcoord, attribute texcoord, );
 rtDeclareVariable(float3, geometric_normal, attribute geometric_normal, );
 rtDeclareVariable(float3, shading_normal, attribute shading_normal, );
-rtDeclareVariable(optix::Ray, ray, rtCurrentRay, );
 
-template<bool use_robust_method>
-static __device__
+static __device__ float3 cylindernormal(float t, float3 t0, float3 t1)
+{
+  float3 neg = make_float3(t==t0.x?1:0, t==t0.y?1:0, t==t0.z?1:0);
+  float3 pos = make_float3(t==t1.x?1:0, t==t1.y?1:0, t==t1.z?1:0);
+  return pos-neg;
+}
+
+// cylinder intersection
+// Following intersection maths described ub RTCD - Christer Ericson
+// Define
+/**
+ * @brief Calculate if Cylinder intersection
+ * NOTE: Follows intersection maths described in RTCD - Christer Ericson so
+ * cylinder origin is at P.
+ *
+ *
+ * Define cylinder as P,Q,r                Define ray as A,B
+ * <--r-|
+ * +----Q----*                          A -------------- B
+ * |         |
+ * |         |
+ * +----P----+
+ *
+ * If X is a point on the cylinder surface then
+ * (v - w) . (v - w) - r2 = 0
+ * where;
+ * v = X - P, d = Q - P, w = ((v.d)/(d.d)) . d
+ *
+ * Intersection
+ *       +---------+             L(t) = A + t(B-A)
+ *       |         |
+ *   A---|---------|---B
+ *       |         |
+ *       +---------+
+ * Intersection defined as L(t) = X
+ *
+ */
 RT_PROGRAM void intersect(int)
 {
-  float3 center = make_float3(sphere);
-  float3 O = ray.origin - center;
-  float  l = 1 / length(ray.direction);
-  float3 D = ray.direction * l;
-  float radius = sphere.w;
+  // TODO: Add second half of intersection
 
-  float b = dot(O, D);
-  float c = dot(O, O)-radius*radius;
-  float disc = b*b-c;
-  if(disc > 0.0f){
-    float sdisc = sqrtf(disc);
-    float root1 = (-b - sdisc);
+  // Cylinder information
+  float3 p_loc = cylinder_min; // P location
+  float3 q_loc = cylinder_max; // Q location
+  //float z = cylinder_max.z - cylinder_min.z;
+  float r = cylinder_r.x;
+  float3 d = (q_loc - p_loc) / ray.direction;
 
-    bool do_refine = false;
+  // Ray information
+  float3 m = ray.origin - p_loc; // ray origin relative to P
+  float3 n = ray.direction;
 
-    float root11 = 0.0f;
+  // Other vars
+  bool check_second = true;
 
-    if(use_robust_method && fabsf(root1) > 10.f * radius) {
-      do_refine = true;
-    }
+  // Calculate dot products
+  float md = dot(m,d);
+  float nd = dot(n,d);
+  float dd = dot(d,d);
+  float nn = dot(n,n);
+  float mn = dot(m,n);
+  float mm = dot(m,m);
+  float a = dd * nn - nd * nd;
+  float k = mm - r*r;
+  float c = dd * k - md * md;
+  float t;
 
-    if(do_refine) {
-      // refine root1
-      float3 O1 = O + root1 * D;
-      b = dot(O1, D);
-      c = dot(O1, O1) - radius*radius;
-      disc = b*b - c;
-
-      if(disc > 0.0f) {
-        sdisc = sqrtf(disc);
-        root11 = (-b - sdisc);
-      }
-    }
-
-    bool check_second = true;
-    if( rtPotentialIntersection( (root1 + root11) * l ) ) {
-      shading_normal = geometric_normal = (O + (root1 + root11)*D)/radius;
-      if(rtReportIntersection(0))
-        check_second = false;
-    }
-    if(check_second) {
-      float root2 = (-b + sdisc) + (do_refine ? root1 : 0);
-      if( rtPotentialIntersection( root2 * l ) ) {
-        shading_normal = geometric_normal = (O + root2*D)/radius;
-        rtReportIntersection(0);
-      }
-    }
+  // Test if fully outside endcaps of cylinder
+  if (md < 0.0f && md + nd < 0.0f) {
+    // Not in cylinder
+    // Below P
+    return;
   }
+  else if (md > dd && md + nd > dd) {
+    // Not in cylinder
+    // Above P and Q
+    return;
+  }
+  // Is within endcaps
+  else if (fabs(a) < 1e-6f) {
+    if (c > 0.f) {
+      // Not in cyclinder
+      // 'a' is outside cylinder
+      return;
+    }
+    // Check if endcap intersect
+    if (md < 0.f) {
+      // Intersect with P
+      t = -mn / nn;
+      return;
+    }
+    else if (md > dd) {
+      // Intersect with Q
+      t = (nd - mn) / nn;
+      return;
+    }
+    else {
+      // 'a' is inside cylinder
+      if (rtPotentialIntersection(md)) {
+        shading_normal = geometric_normal = cylindernormal(md, p_loc, q_loc);
+        if(rtReportIntersection(0)) {
+          check_second = false;
+        }
+      }
+    }
+
+  }
+  return;
 }
 
 RT_PROGRAM void bounds (int, float result[6])
 {
-  const float3 cen = make_float3( sphere );
-  const float3 rad = make_float3( sphere.w );
-
   optix::Aabb* aabb = (optix::Aabb*)result;
-
-  if( rad.x > 0.0f  && !isinf(rad.x) ) {
-    aabb->m_min = cen - rad;
-    aabb->m_max = cen + rad;
-  } else {
-    aabb->invalidate();
-  }
+  aabb->set(cylinder_min, cylinder_max);
 }
