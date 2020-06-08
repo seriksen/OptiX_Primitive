a4b9b4f1d3bbadd4c0a6e20fc4accae71b9a9ab3
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-06-08 17:48:09 +0100

    Changing variable names

--- OptiXCylinder/OptiXCylinder.cc
+++ OptiXCylinder/OptiXCylinder.cc
@@ -181,9 +181,9 @@ optix::GeometryInstance createCylinder(optix::Context context,
 
   // Set box size
   float sz = ce.w;
-  cylinder["cylinder_min"]->setFloat(-sz / 2.f, -sz / 2.f, -sz / 2.f);
-  cylinder["cylinder_max"]->setFloat(sz / 2.f, sz / 2.f, sz / 2.f);
-  cylinder["cylinder_r"]->setFloat(sz/2.f,0.f,0.f);
+  cylinder["cylinder_p"]->setFloat(0.f, 0.f, -sz / 2.f);
+  cylinder["cylinder_q"]->setFloat(0.f, 0.f, sz / 2.f);
+  cylinder["cylinder_r"]->setFloat(0.f,0.f,0.f, 0.25f);
 
   // Put it all together
   optix::GeometryInstance gi =
