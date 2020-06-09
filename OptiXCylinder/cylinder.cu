765586eb39e72c752a598647141f2aa0834a3f71
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-06-09 04:49:13 +0100

    Testing out shading

--- OptiXCylinder/cylinder.cu
+++ OptiXCylinder/cylinder.cu
@@ -238,7 +238,7 @@ RT_PROGRAM void intersect(int) {
     {
       if( rtPotentialIntersection(t) )
       {
-        shading_normal = geometric_normal = -normalize(d);
+        shading_normal = geometric_normal = normalize(d);
         rtReportIntersection(0);
       }
     }
