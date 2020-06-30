31ba3aaad1154fae1f5ec3abc650a3b0388941fb
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-06-30 19:59:09 +0100

    testing moving point

--- OptiXDisc/OneHole/disc.cu
+++ OptiXDisc/OneHole/disc.cu
@@ -48,7 +48,7 @@ RT_PROGRAM void intersect(int) {
   float hole_rsq = hole_t_center*(2.f*hole_mn + hole_t_center*nn) + hole_mm;
 
   // TODO let hole not be in center
-  float t_cand = (rsq < rr && hole_rqs > hole_rr) ? t_center : t_min;
+  float t_cand = (rsq < rr && hole_rsq > hole_rr) ? t_center : t_min;
 
 
   bool valid_isect = t_cand > t_min ;
