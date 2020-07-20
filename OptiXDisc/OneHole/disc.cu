b27d6b5940d66fb70c6359c53f2609687ea17d49
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2020-07-20 20:01:16 +0100

    debugging onehole

--- OptiXDisc/OneHole/disc.cu
+++ OptiXDisc/OneHole/disc.cu
@@ -70,7 +70,7 @@ RT_PROGRAM void intersect(int) {
   float3 o = ray.origin;
 
   // t
-  float t = dot((c - o), n) / dot (n,d);
+  float t = dot((c - o), n) / dot (d,n);
   float t_min = 0.f;
 
   // check if intersects
@@ -87,12 +87,12 @@ RT_PROGRAM void intersect(int) {
 
   if (r_sq < rr && t > t_min) {
     // Now check hole
-    float t_hole = - dot((o - hole_c), n) / dot(n,d);
+    float t_hole = - dot((o - hole_c), n) / dot(d,n);
     float r_sq_h = t_hole * (2.f * dot((o - hole_c), d) + t_hole * dot(d,d))
                               + dot(o - hole_c,o - hole_c);
     float hole_rr = hole_r*hole_r;
     if (r_sq_h > hole_rr && t_hole > t_min) {
-      if (rtPotentialIntersection(t)) {
+      if (rtPotentialIntersection(t_hole)) {
         shading_normal = geometric_normal = normalize(n);
         rtReportIntersection(0);
       }
